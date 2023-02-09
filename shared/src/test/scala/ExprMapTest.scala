072ee524efe7cd2f62af8c13d2aa91b907938258
Adam Vandervorst <adam.vandervorst@gmail.com>
2023-02-09 12:13:39 +0100

    Disqualify parallel representation

--- shared/src/test/scala/ExprMapTest.scala
+++ shared/src/test/scala/ExprMapTest.scala
@@ -284,18 +284,29 @@ class ExprMapTest extends FunSuite:
     assert(res == Set(Expr(posses, Sam, balloon), Expr(likes, Sam, Expr(blue, stuff))))
   }
 
-  test("parallel Expr ExprMap") {
+  test("parallel Expr ExprMap".ignore) {
     // the basic idea is to split an expression into multiple ones based on a symbol, say `|`
     // e.g. (P (| (S Z) (S (S Z)) Z)) roughly becomes {P: {S: {S: Z, Z}, Z}} which is the prefix compression of
     // (P (S Z)); (P (S (S Z))); (P Z)
 
     val | = Var(1000)
     val p1 = Expr(A, Expr(|, Expr(B, C), Expr(|, Expr(B, Expr(B, C)), C)))
+    val p2 = Expr(Expr(|, f, Expr(h, g)), Expr(|, A, Expr(B, A)))
 
-//    val em = ExprMap(
-//      Expr(`=`, Expr(f, Expr(|, $, $)), _1) -> 0
-//      Expr(`=`, Expr(f, Expr(|, $, $)), _2) -> 1
-//
-//
-//    )
+    val em2 = ExprMap(
+      Expr(f, A) -> 0,
+      Expr(f, Expr(B, A)) -> 10,
+      Expr(Expr(h, g), A) -> 1,
+      Expr(Expr(h, g), Expr(B, A)) -> 11,
+    )
+
+    // Yikes! The ExprMap way of compressing terms duplicates the A | (B A)
+    // println(em2.prettyStructuredSet())
+    /* ⦑⧼1: ⧼20|⧼21: ⧼20⦒⦒⧽|⧼3: ⧼2: ⧼20|⧼21: ⧼20⦒⦒⧽⦒⦒⧽⧽ */
+
+    // Potential transormation
+    // val fem = ExprMap(
+    //   Expr(`=`, Expr(f, Expr(|, $, $)), _1) -> 0
+    //   Expr(`=`, Expr(f, Expr(|, $, $)), _2) -> 1
+    // )
   }
