7bf7803c27ae9e063121a076d3eda446ff8c390d
Adam Vandervorst <adam.vandervorst@gmail.com>
2024-03-21 10:56:11 +0100

    Move final to classes

--- shared/src/main/scala/ExprMap.scala
+++ shared/src/main/scala/ExprMap.scala
@@ -45,20 +45,20 @@ private sealed trait EMImpl[V, F[_]]:
   def nonEmpty: Boolean = !isEmpty
 end EMImpl
 
-case class EM[V](apps: ExprMap[ExprMap[V]],
-                 var vars: VarMap[V]) extends EMImpl[V, EM]:
-  final def copy(): EM[V] = EM(apps.copy(), vars.copy())
+final case class EM[V](apps: ExprMap[ExprMap[V]],
+                   var vars: VarMap[V]) extends EMImpl[V, EM]:
+  def copy(): EM[V] = EM(apps.copy(), vars.copy())
 
-  final def contains(e: Expr): Boolean = e match
+  def contains(e: Expr): Boolean = e match
     case Var(i) => vars.contains(i)
     case App(f, a) => apps.get(f).fold(false)(_.contains(a))
 
-  final def updated(e: Expr, v: V): EM[V] =
+  def updated(e: Expr, v: V): EM[V] =
     val c = this.copy()
     c.update(e, v)
     c
 
-  final def update(e: Expr, v: V): Unit = e match
+  def update(e: Expr, v: V): Unit = e match
     case Var(i) =>
       vars = vars.updated(i, v)
     case App(f, a) =>
@@ -66,7 +66,7 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
         gapp => gapp.update(a, v); gapp
       }
 
-  final def updateWithDefault(e: Expr)(default: => V)(remap: V => V): Unit = e match
+  def updateWithDefault(e: Expr)(default: => V)(remap: V => V): Unit = e match
     case Var(i) =>
       vars = vars.updatedWithDefault(i, default, remap)
     case App(f, a) =>
@@ -74,7 +74,7 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
         gapp => gapp.updateWithDefault(a)(default)(remap); gapp
       }
 
-  final def updateWith(e: Expr)(remap: Option[V] => Option[V]): Unit = e match
+  def updateWith(e: Expr)(remap: Option[V] => Option[V]): Unit = e match
     case Var(i) =>
       vars.updatedWith(i)(remap)
     case App(f, a) =>
@@ -83,20 +83,20 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
         case None => val c = ExprMap[V](); c.updateWith(a)(remap); Some(c)
       }
 
-  final def remove(e: Expr): Option[V] = e match
+  def remove(e: Expr): Option[V] = e match
     case Var(i) => val v = vars.get(i); vars = vars.removed(i); v
     case App(f, a) =>
       apps.get(f).flatMap(_.remove(a))
 
-  final def getUnsafe(e: Expr): V = e match
+  def getUnsafe(e: Expr): V = e match
     case Var(i) => vars(i)
     case App(f, a) => apps.getUnsafe(f).getUnsafe(a)
 
-  final def get(e: Expr): Option[V] = e match
+  def get(e: Expr): Option[V] = e match
     case Var(i) => vars.get(i)
     case App(f, a) => apps.get(f).flatMap(_.get(a))
 
-  final def getAt(ins: IterableOnce[Option[Int]]): Either[EM[_], V] =
+  def getAt(ins: IterableOnce[Option[Int]]): Either[EM[_], V] =
     var loc: EM[_] = this
     var apps = 0
     var it = ins.iterator
@@ -121,61 +121,61 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
         apps += 1
     Left(loc)
 
-  final def keys: Iterable[Expr] =
+  def keys: Iterable[Expr] =
     val ks = mutable.ArrayDeque.empty[Expr]
     foreachKey(ks.append)
     ks
 
-  final def items: Iterable[(Expr, V)] =
+  def items: Iterable[(Expr, V)] =
     val ks = mutable.ArrayDeque.empty[(Expr, V)]
     foreachItem((k, v) => ks.append(k -> v))
     ks
 
-  final def values: Iterable[V] =
+  def values: Iterable[V] =
     val vs = mutable.ArrayDeque.empty[V]
     foreach(vs.append)
     vs
 
-  final def union(that: EM[V]): EM[V] =
+  def union(that: EM[V]): EM[V] =
     EM(
       this.apps.unionWith(_ union _)(that.apps),
       this.vars.union(that.vars)
     )
 
-  final def unionWith(op: (V, V) => V)(that: EM[V]): EM[V] =
+  def unionWith(op: (V, V) => V)(that: EM[V]): EM[V] =
     EM(
       this.apps.unionWith(_.unionWith(op)(_))(that.apps),
       this.vars.unionWith(that.vars, (_, l, r) => op(l, r))
     )
 
-  final def intersection(that: EM[V]): ExprMap[V] =
+  def intersection(that: EM[V]): ExprMap[V] =
     val vs = vars.intersection(that.vars)
       .filter{ case (_, v: ExprMap[_]) => v.nonEmpty; case _ => true } // TODO hack
     val as = this.apps.intersectionWith(_.intersection(_))(that.apps)
     ExprMap[V](if vs.isEmpty && as.isEmpty then null else EM(as, vs))
 
-  final def intersectionWith(op: (V, V) => V)(that: EM[V]): ExprMap[V] =
+  def intersectionWith(op: (V, V) => V)(that: EM[V]): ExprMap[V] =
     val vs = vars.intersectionWith(that.vars, (_, l, r) => op(l, r))
       .filter{ case (_, v: ExprMap[_]) => v.nonEmpty; case _ => true } // TODO hack
     val as = this.apps.intersectionWith(_.intersectionWith(op)(_))(that.apps)
     ExprMap[V](if vs.isEmpty && as.isEmpty then null else EM(as, vs))
 
-  final def subtract(that: EM[V]): ExprMap[V] =
+  def subtract(that: EM[V]): ExprMap[V] =
     val vs = vars.subtract(that.vars)
     val as = this.apps.subtractWith((x, y) => Some(x.subtract(y)).filter(_.nonEmpty))(that.apps)
     ExprMap[V](if vs.isEmpty && as.isEmpty then null else EM(as, vs))
 
-  final def subtractWith(p: (V, V) => Option[V])(that: EM[V]): ExprMap[V] =
+  def subtractWith(p: (V, V) => Option[V])(that: EM[V]): ExprMap[V] =
     val vs = vars.subtractWith(p)(that.vars)
     val as = this.apps.subtractWith((x, y) => Some(x.subtractWith(p)(y)).filter(_.nonEmpty))(that.apps)
     ExprMap[V](if vs.isEmpty && as.isEmpty then null else EM(as, vs))
 
-  final def map[W](f: V => W): EM[W] = EM(
+  def map[W](f: V => W): EM[W] = EM(
     apps.map(_.map(f)),
     vars.mapValuesNow(f)
   )
 
-  final def collect[W](pf: PartialFunction[V, W]): EM[W] =
+  def collect[W](pf: PartialFunction[V, W]): EM[W] =
     EM(
       apps.collect(((em: ExprMap[V]) =>
         val r = em.collect(pf)
@@ -216,7 +216,7 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
   //            pat_var_bndr union pat_var_occs union look_at_e
 
 
-  final def indiscriminateMatching(e: Expr): ExprMap[V] = e match
+  def indiscriminateMatching(e: Expr): ExprMap[V] = e match
     case Var(i) if i > 0 => vars.get(i).fold(ExprMap())(x => ExprMap.single(Var(i), x))
     case Var(_) => ExprMap(this)
     case App(f, a) =>
@@ -224,7 +224,7 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
       val lv2: ExprMap[ExprMap[V]] = lv1.map[ExprMap[V]](_.indiscriminateMatching(a))
       ExprMap(EM(lv2, VarMap.empty))
 
-  final def indiscriminateReverseMatching(e: Expr): ExprMap[V] = e match
+  def indiscriminateReverseMatching(e: Expr): ExprMap[V] = e match
     case Var(i) => ExprMap(EM(ExprMap(), vars.filter((j, _) => j <= 0 || j == i)))
     case App(f, a) =>
       val lv1: ExprMap[ExprMap[V]] = apps.indiscriminateReverseMatching(f)
@@ -233,7 +233,7 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
         nem.indiscriminateReverseMatching(a)
       }, vars.filter((j, _) => j <= 0)))
 
-  final def indiscriminateBidirectionalMatching(e: Expr): ExprMap[V] = e match
+  def indiscriminateBidirectionalMatching(e: Expr): ExprMap[V] = e match
     case Var(i) if i > 0 =>
 //      println(s"removing apps and symbols != ${i} from ${this.prettyStructuredSet()}")
       ExprMap(EM(ExprMap(), vars.filter((j, _) => j <= 0 || j == i)))
@@ -247,14 +247,14 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
       ).unlift), vars.filter((j, _) => j <= 0)))
 
 
-  final def transform(pattern: Expr, template: Expr): ExprMap[V] =
+  def transform(pattern: Expr, template: Expr): ExprMap[V] =
     val possible = indiscriminateBidirectionalMatching(pattern)
 
     ExprMap.from(possible.items.collect(((x: (Expr, V)) =>
       util.Try(x._1.transform(pattern, template) -> x._2).toOption
     ).unlift))
 
-  final def transformMatches(pattern: Expr, template: Expr): ExprMap[V] =
+  def transformMatches(pattern: Expr, template: Expr): ExprMap[V] =
     val possible = indiscriminateBidirectionalMatching(pattern)
 
     val res = ExprMap[V]()
@@ -265,41 +265,41 @@ case class EM[V](apps: ExprMap[ExprMap[V]],
 
   inline def execute(instrs: IterableOnce[Instr]): ExprMap[V] = ExprMap(this).execute(instrs)
 
-  final def flatMap[W](op: (W, W) => W)(f: V => ExprMap[W]): ExprMap[W] =
+  def flatMap[W](op: (W, W) => W)(f: V => ExprMap[W]): ExprMap[W] =
     var nem = apps.flatMap(op)(_.flatMap(op)(f))
     vars.foreachValue(v => nem = nem.unionWith(op)(f(v)))
     nem
 
-  final def foldRight[R](z: R)(op: (V, R) => R): R =
+  def foldRight[R](z: R)(op: (V, R) => R): R =
     var a = z
     apps.foreach{ v => a = v.foldRight(a)(op) }
     vars.foreachValue{ v => a = op(v, a) }
     a
 
-  final def foreachKey(func: Expr => Unit): Unit =
+  def foreachKey(func: Expr => Unit): Unit =
     vars.foreachKey(k => func(Var(k.toInt)))
     apps.foreachItem((f, em) =>
       em.foreachKey(a => func(App(f, a)))
     )
 
-  final def foreachItem(func: (Expr, V) => Unit): Unit =
+  def foreachItem(func: (Expr, V) => Unit): Unit =
     vars.foreachEntry((k, v) => func(Var(k.toInt), v))
     apps.foreachItem((f, em) =>
       em.foreachItem((a, v) => func(App(f, a), v))
     )
 
-  final def foreach(f: V => Unit): Unit =
+  def foreach(f: V => Unit): Unit =
     vars.foreachValue(f)
     apps.foreach(_.foreach(f))
 
-  final def size: Int = foldRight(0)((_, c) => c + 1)
+  def size: Int = foldRight(0)((_, c) => c + 1)
 
   inline def prettyStructured(colored: Boolean = true, tree: Boolean = false): String = ExprMap(this).prettyStructured(colored, tree)
   inline def prettyStructuredSet(colored: Boolean = true, tree: Boolean = false): String = ExprMap(this).prettyStructuredSet(colored, tree)
   inline def prettyListing(colored: Boolean = true): String = ExprMap(this).prettyListing(colored)
   inline def json: String = ExprMap(this).json
 
-  final def isEmpty: Boolean = vars.isEmpty && apps.isEmpty
+  def isEmpty: Boolean = vars.isEmpty && apps.isEmpty
 end EM
 
 object EM:
@@ -322,69 +322,69 @@ object EM:
 //      VarMap.empty
 //    )
 
-case class ExprMap[V](var em: EM[V] = null) extends EMImpl[V, ExprMap]:
-  final def copy(): ExprMap[V] = if em eq null then ExprMap() else ExprMap(em.copy())
-  final def contains(e: Expr): Boolean = if em eq null then false else em.contains(e)
+final case class ExprMap[V](var em: EM[V] = null) extends EMImpl[V, ExprMap]:
+  def copy(): ExprMap[V] = if em eq null then ExprMap() else ExprMap(em.copy())
+  def contains(e: Expr): Boolean = if em eq null then false else em.contains(e)
   inline def getUnsafe(e: Expr): V = em.getUnsafe(e)
-  final def get(e: Expr): Option[V] = if em eq null then None else em.get(e)
-  final def getAt(ins: IterableOnce[Option[Int]]): Either[EM[_], V] = if em eq null then ins.iterator.nextOption().fold(Left(null)){ ins => throw RuntimeException(s"tried to execute ${ins} on empty") } else em.getAt(ins)
-  final def updated(e: Expr, v: V): ExprMap[V] = ExprMap(if em eq null then EM.single(e, v) else em.updated(e, v))
+  def get(e: Expr): Option[V] = if em eq null then None else em.get(e)
+  def getAt(ins: IterableOnce[Option[Int]]): Either[EM[_], V] = if em eq null then ins.iterator.nextOption().fold(Left(null)){ ins => throw RuntimeException(s"tried to execute ${ins} on empty") } else em.getAt(ins)
+  def updated(e: Expr, v: V): ExprMap[V] = ExprMap(if em eq null then EM.single(e, v) else em.updated(e, v))
   inline def update(e: Expr, v: V): Unit = if em eq null then em = EM.single(e, v) else em.update(e, v)
   inline def updateWithDefault(e: Expr)(default: => V)(f: V => V): Unit = if em eq null then em = EM.single(e, default) else em.updateWithDefault(e)(default)(f)
-  final def updateWith(e: Expr)(f: Option[V] => Option[V]): Unit = if em eq null then f(None).foreach(v => em = EM.single(e, v)) else em.updateWith(e)(f)
-  final def remove(e: Expr): Option[V] = if em eq null then None else em.remove(e)
-  final def keys: Iterable[Expr] = if em eq null then Iterable.empty else em.keys
-  final def values: Iterable[V] = if em eq null then Iterable.empty else em.values
-  final def items: Iterable[(Expr, V)] = if em eq null then Iterable.empty else em.items
+  def updateWith(e: Expr)(f: Option[V] => Option[V]): Unit = if em eq null then f(None).foreach(v => em = EM.single(e, v)) else em.updateWith(e)(f)
+  def remove(e: Expr): Option[V] = if em eq null then None else em.remove(e)
+  def keys: Iterable[Expr] = if em eq null then Iterable.empty else em.keys
+  def values: Iterable[V] = if em eq null then Iterable.empty else em.values
+  def items: Iterable[(Expr, V)] = if em eq null then Iterable.empty else em.items
   inline def union(that: ExprMap[V]): ExprMap[V] = if em eq null then that.copy() else if that.em eq null then this.copy() else ExprMap(this.em.union(that.em))
   inline def unionWith(op: (V, V) => V)(that: ExprMap[V]): ExprMap[V] = if em eq null then that.copy() else if that.em eq null then this.copy() else ExprMap(this.em.unionWith(op)(that.em))
   inline def intersection(that: ExprMap[V]): ExprMap[V] = if (em eq null) || (that.em eq null) then ExprMap() else this.em.intersection(that.em)
   inline def intersectionWith(op: (V, V) => V)(that: ExprMap[V]): ExprMap[V] = if (em eq null) || (that.em eq null) then ExprMap() else this.em.intersectionWith(op)(that.em)
   inline def subtract(that: ExprMap[V]): ExprMap[V] = if em eq null then this.copy() else if that.em eq null then this.copy() else this.em.subtract(that.em)
   inline def subtractWith(p: (V, V) => Option[V])(that: ExprMap[V]): ExprMap[V] = if em eq null then this.copy() else if that.em eq null then this.copy() else this.em.subtractWith(p)(that.em)
-  final def foreachKey(f: Expr => Unit): Unit = if em ne null then em.foreachKey(f)
-  final def foreachItem(f: (Expr, V) => Unit): Unit = if em ne null then em.foreachItem(f)
-  final def foreach(f: V => Unit): Unit = if em ne null then em.foreach(f)
-  final def map[W](f: V => W): ExprMap[W] = ExprMap(if em eq null then null else em.map(f))
-  final def collect[W](pf: PartialFunction[V, W]): ExprMap[W] = ExprMap(if em eq null then null else em.collect(pf))
-  final def indiscriminateMatching(e: Expr): ExprMap[V] = if em eq null then ExprMap() else em.indiscriminateMatching(e)
-  final def indiscriminateReverseMatching(e: Expr): ExprMap[V] = if em eq null then ExprMap() else em.indiscriminateReverseMatching(e)
-  final def indiscriminateBidirectionalMatching(e: Expr): ExprMap[V] = if em eq null then ExprMap() else em.indiscriminateBidirectionalMatching(e)
+  def foreachKey(f: Expr => Unit): Unit = if em ne null then em.foreachKey(f)
+  def foreachItem(f: (Expr, V) => Unit): Unit = if em ne null then em.foreachItem(f)
+  def foreach(f: V => Unit): Unit = if em ne null then em.foreach(f)
+  def map[W](f: V => W): ExprMap[W] = ExprMap(if em eq null then null else em.map(f))
+  def collect[W](pf: PartialFunction[V, W]): ExprMap[W] = ExprMap(if em eq null then null else em.collect(pf))
+  def indiscriminateMatching(e: Expr): ExprMap[V] = if em eq null then ExprMap() else em.indiscriminateMatching(e)
+  def indiscriminateReverseMatching(e: Expr): ExprMap[V] = if em eq null then ExprMap() else em.indiscriminateReverseMatching(e)
+  def indiscriminateBidirectionalMatching(e: Expr): ExprMap[V] = if em eq null then ExprMap() else em.indiscriminateBidirectionalMatching(e)
   //  def matching(e: Expr, tracker: ExprMap[mutable.ArrayDeque[Int]] = ExprMap()): ExprMap[V] = if em == null then ExprMap() else em.matching(e, tracker)
-  final def execute(instrs: IterableOnce[Instr], debug: 0 | 1 | 2 = 0): ExprMap[V] = new ExprMapEngine[V].execute(this, instrs, debug)
-  final def transform(pattern: Expr, template: Expr): ExprMap[V] = if em eq null then ExprMap() else em.transform(pattern, template)
-  final def transformMatches(pattern: Expr, template: Expr): ExprMap[V] = if em eq null then ExprMap() else em.transformMatches(pattern, template)
-  final def flatMap[W](op: (W, W) => W)(f: V => ExprMap[W]): ExprMap[W] = if em eq null then ExprMap() else em.flatMap(op)(f)
-  final def foldRight[R](z: R)(op: (V, R) => R): R = if em eq null then z else em.foldRight(z)(op)
+  def execute(instrs: IterableOnce[Instr], debug: 0 | 1 | 2 = 0): ExprMap[V] = new ExprMapEngine[V].execute(this, instrs, debug)
+  def transform(pattern: Expr, template: Expr): ExprMap[V] = if em eq null then ExprMap() else em.transform(pattern, template)
+  def transformMatches(pattern: Expr, template: Expr): ExprMap[V] = if em eq null then ExprMap() else em.transformMatches(pattern, template)
+  def flatMap[W](op: (W, W) => W)(f: V => ExprMap[W]): ExprMap[W] = if em eq null then ExprMap() else em.flatMap(op)(f)
+  def foldRight[R](z: R)(op: (V, R) => R): R = if em eq null then z else em.foldRight(z)(op)
 
-  private final def appliedRec[W](fem: ExprMap[W], aem: ExprMap[W]): ExprMap[ExprMap[W]] =
+  private def appliedRec[W](fem: ExprMap[W], aem: ExprMap[W]): ExprMap[ExprMap[W]] =
     ExprMap(if fem.em eq null then null else EM[ExprMap[W]](
       ExprMap(if fem.em.apps.em eq null then null else EM[ExprMap[ExprMap[W]]](
         appliedRec(fem.em.apps.em.apps, aem.asInstanceOf),
         fem.em.apps.em.vars.mapValuesNow(appliedRec(_, aem)))),
       fem.em.vars.mapValuesNow(_ => aem)))
 
-  final def applied(aem: ExprMap[V]): ExprMap[V] =
+  def applied(aem: ExprMap[V]): ExprMap[V] =
     ExprMap(EM(appliedRec(this, aem), VarMap.empty))
 
-  private final def appliedWithRec[W](op: (W, W) => W)(fem: ExprMap[W], aem: ExprMap[W]): ExprMap[ExprMap[W]] =
+  private def appliedWithRec[W](op: (W, W) => W)(fem: ExprMap[W], aem: ExprMap[W]): ExprMap[ExprMap[W]] =
     ExprMap(if fem.em eq null then null else EM(
       ExprMap(if fem.em.apps.em eq null then null else EM(
         appliedWithRec[ExprMap[ExprMap[W]]](_.unionWith(_.unionWith(op)(_))(_))(fem.em.apps.em.apps, aem.asInstanceOf),
         fem.em.apps.em.vars.mapValuesNow(em => appliedWithRec(op)(em, aem)))),
       fem.em.vars.mapValuesNow(w1 => aem.map(w2 => op(w1, w2)))))
 
-  final def appliedWith(op: (V, V) => V)(aem: ExprMap[V]): ExprMap[V] =
+  def appliedWith(op: (V, V) => V)(aem: ExprMap[V]): ExprMap[V] =
     ExprMap(EM(appliedWithRec(op)(this, aem), VarMap.empty))
 
-  final def appliedWithSim[W](op: (W, W) => W)(fem: ExprMap[W])(aem: ExprMap[W]): ExprMap[W] =
+  def appliedWithSim[W](op: (W, W) => W)(fem: ExprMap[W])(aem: ExprMap[W]): ExprMap[W] =
     ExprMap.from(fem.items.flatMap((fe, fw) => aem.items.map((ae, aw) => Expr(fe, ae) -> op(fw, aw))))
 
-  final def size: Int = if em eq null then 0 else foldRight(0)((_, c) => c + 1)
-  final def prettyStructured(colored: Boolean = true, tree: Boolean = false): String = EMPrettyPrinter.structured(this, colored=colored, tree=tree)
-  final def prettyStructuredSet(colored: Boolean = true, tree: Boolean = false): String = EMPrettyPrinter.structuredSet(this, colored=colored, tree=tree)
-  final def prettyListing(colored: Boolean = true): String = EMListPrinter.listing(this, colored=colored)
-  final def json: String = EMJSONPrinter.structured(this, colored=false)
+  def size: Int = if em eq null then 0 else foldRight(0)((_, c) => c + 1)
+  def prettyStructured(colored: Boolean = true, tree: Boolean = false): String = EMPrettyPrinter.structured(this, colored=colored, tree=tree)
+  def prettyStructuredSet(colored: Boolean = true, tree: Boolean = false): String = EMPrettyPrinter.structuredSet(this, colored=colored, tree=tree)
+  def prettyListing(colored: Boolean = true): String = EMListPrinter.listing(this, colored=colored)
+  def json: String = EMJSONPrinter.structured(this, colored=false)
   inline def isEmpty: Boolean = (em eq null) || em.isEmpty
 end ExprMap
 
