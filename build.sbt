c55b4aeb607cad7d25a8bb83cc087b8d62ddcce5
Adam-Vandervorst <adam.vandervorst@gmail.com>
2025-02-06 12:32:31 +0100

    par interner

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ import scala.scalanative.build.*
 import org.scalajs.linker.interface.ESVersion
 
 
-ThisBuild / version := "0.2.14"
+ThisBuild / version := "0.2.16"
 ThisBuild / organization := "be.adamv"
 
 ThisBuild / scalaVersion := "3.4.2"
--- shared/src/main/scala/RangeStorage.scala
+++ shared/src/main/scala/RangeStorage.scala
@@ -1,5 +1,7 @@
 package be.adamv.cz2
 
+import java.util.concurrent.atomic.AtomicInteger
+
 
 class RangeStorage[A](val start: Int, val end: Int):
   val size = end - start
@@ -63,3 +65,60 @@ object RangeStorage:
 object Vars:
   def unapplySeq(ns: RangeStorage[Int]): Some[LazyList[Expr]] =
     Some(LazyList.tabulate(ns.free)(ns.addV))
+
+
+class ParRangeStorage[A](val start: Int, val end: Int):
+  val size = end - start
+  assert(size > 0)
+  var _occupied = AtomicInteger(0)
+  def occupied = _occupied.get()
+  def occupied_=(i: Int) = _occupied.set(i)
+
+  def free: Int = size - occupied
+
+  val valueToIndex = java.util.concurrent.ConcurrentHashMap[A, Integer]()
+  val indexToValue = java.util.concurrent.ConcurrentHashMap[Integer, A]()
+  val children = java.util.concurrent.ConcurrentLinkedQueue[RangeStorage[_]]
+
+  extension (inline sc: StringContext)(using inline ev: String =:= A)
+    inline def v(inline args: Any*): Expr =
+      val k = ev(StringContext.standardInterpolator(identity, args, sc.parts))
+      Var(this.lookup(k).getOrElse(this.add(k)))
+
+  def inRange[B](size: Int): RangeStorage[B] =
+    assert(size <= free)
+    val newNS = new RangeStorage[B](start + occupied, start + occupied + size)
+    children.add(newNS)
+    occupied += size
+    newNS
+
+  def storeInRange[B](elems: Set[B]): RangeStorage[B] =
+    assert(elems.size <= free)
+    val newNS = new RangeStorage[B](start + occupied, start + occupied + elems.size)
+    children.add(newNS)
+    occupied += elems.size
+    for elem <- elems do newNS.add(elem)
+    newNS
+
+  def add(a: A): Int =
+    assert(free > 0)
+    valueToIndex.computeIfAbsent(a, _ => {
+      val i = start + _occupied.incrementAndGet()
+      indexToValue.put(i, a)
+      i
+    })
+
+  def addV(a: A): Expr =
+    Expr.Var(add(a))
+
+  def couldContain(i: Int): Boolean =
+    i < end && i >= start
+
+  def get(i: Int): Option[A] =
+    Option(indexToValue.get(i))
+
+  def lookup(s: A): Option[Int] =
+    Option(valueToIndex.get(s))
+
+  inline def present: Range =
+    Range(start, start + occupied)
